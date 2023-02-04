dae681aba171c3e80b49797d9eaefeef73dcf6c6
Deepam Das <109923289+Deepamdas23@users.noreply.github.com>
2023-02-05 05:38:58 +1100

    Add files via upload
    Module 7

---
+++ Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -0,0 +1,47 @@
+--Table with retirees and their titles
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,ackard-Analysis\Pewlett-Hackard-ackard-Analysis\Pewlett-Hackard-
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+--Table with unique list of employees from retirement table
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE rt.to_date = '9999-01-01'
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+--Table with retiree counts by title
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT (ut.title) DESC;
+
+--Table with mentorish-eligibility employees
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+       e.first_name,
+       e.last_name,
+       e.birth_date,
+       de.from_date,
+       de.to_date,
+       t.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+LEFT JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE t.to_date = '9999-01-01' 
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31';
